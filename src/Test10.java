37627cf718e155b89908b6f6f983735b1aeb8da9
haroldCoder <haroldc2005@gmail.com>
2024-02-02 17:57:36 -0500

    terminate Test 10

--- src/Test10.java
+++ src/Test10.java
@@ -1,11 +1,38 @@
-import org.openqa.selenium.JavascriptExecutor;
+import static org.junit.Assert.assertTrue;
 
-public class Test10 implements App {
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class Test10 extends App {
     void NavigateFooter(){
+        WebElement formSubs = driver.findElement(By.className("footer-widget"));
+
         JavascriptExecutor js = (JavascriptExecutor) this.driver;
 
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-
-        Thread.sleep(300);
+        js.executeScript("arguments[0].scrollIntoView(true);", formSubs);
     }
+
+    @Test
+    public void VerifyTextSubscript(){
+        this.NavigateFooter();
+        assertTrue(driver.findElement(By.xpath("//div[contains(@class, 'single-widget')]")).findElement(By.tagName("h2")).getText() != "");
+    }
+
+    void SignEmail(){
+        this.NavigateFooter();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//input[contains(@placeholder, 'Your email address')]")).sendKeys("koderxc2005@gmail.com");
+        driver.findElement(By.id("subscribe")).click();
+    }
+
+    @Test
+    public void isSubscribedSuccessfully(){
+        this.SignEmail();
+        assertTrue(driver.findElement(By.className("alert-success alert")).isDisplayed());
+    }
+
 }
